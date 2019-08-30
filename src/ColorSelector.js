20e6170ab46683c382c021d6582193fd4c3820e3
Kish-Siva-1 <rodes88@msn.com>
2019-08-30 06:46:28 +0200

    Done.

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 
 export default class ColorSelector extends Component {
   
+  //Click the color swatch and store the color in the Matrix
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+      return <div key={idx} className="color-swatch" style={{backgroundColor: str}} onClick = {() => {this.props.makeColor(str)}} />
     })
   )
   
