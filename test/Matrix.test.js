35cd80a28564671df9b4d5f956dbb337a9405640
Anthea <anthea.mertn@gmail.com>
2019-11-06 11:08:03 +0000

    Done.

--- test/Matrix.test.js
+++ test/Matrix.test.js
@@ -1,20 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { configure, shallow } from 'enzyme';
-import { expect } from 'chai';
+import React from "react";
+import ReactDOM from "react-dom";
+import { configure, shallow } from "enzyme";
+import { expect } from "chai";
 // import sinon from 'sinon';
 
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
 
-import Matrix from '../src/Matrix'
+import Matrix from "../src/Matrix";
 
-
-describe('<Matrix />', () => {
-
+describe("<Matrix />", () => {
   it("change me to true in test/matrix.test.js once your app is working!", () => {
-    expect(false).to.equal(true)
-  })
-
-})
+    expect(true).to.equal(true);
+  });
+});
